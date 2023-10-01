f23ed847fcca7ce05dbd3df56e836e885fec3166
Azat Yanberdin <azatdjon@yandex.ru>
2023-10-01 11:30:53 +0500

    add new changes in my proj

--- books/views.py
+++ books/views.py
@@ -1,9 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin
 )
 from .models import Book
+from django.db.models import Q
 
 
 class BookListView(LoginRequiredMixin, ListView):
@@ -23,3 +26,16 @@ class BookDetailView(
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    queryset = Book.objects.filter(title__icontains='beginners')
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
